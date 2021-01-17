5ee144c20e464bd683a29711ca4c24d95b32078c
Bimal Kumar <bimalkumar.das@angloamerican.com>
2021-01-17 20:14:44 +0530

    -Added test cases

--- FreedomMobileShop.Service.Test/Implementation/MobileStoreServiceTest.cs
+++ FreedomMobileShop.Service.Test/Implementation/MobileStoreServiceTest.cs
@@ -1,14 +1,153 @@
 ﻿/// <summary>
-/// 
+/// Test class file for service layer
 /// </summary>
 namespace FreedomMobileShop.Service.Test.Implementation
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using FreedomMobileShop.Common.Constants;
+    using FreedomMobileShop.Common.Helpers;
+    using FreedomMobileShop.DataAccess.Interface;
+    using FreedomMobileShop.Entity.Entities;
+    using FreedomMobileShop.Service.Helper;
+    using FreedomMobileShop.Service.Implementation;
+    using FreedomMobileShop.Service.Test.TestCaseData;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using Newtonsoft.Json;
+    using Xunit;
 
     public class MobileStoreServiceTest
     {
+        #region Private Variables
 
+        private Mock<IRepositoryWrapper> _mockIServiceWrapper;
+        private ServiceWrapper _serviceWrapper;
+        private ILogger<MobileStoreService> _mobileStoreServiceLogger = Mock.Of<ILogger<MobileStoreService>>();
+
+        #endregion
+
+        public MobileStoreServiceTest()
+        {
+            _mockIServiceWrapper = new Mock<IRepositoryWrapper>();
+            _serviceWrapper = new ServiceWrapper(_mockIServiceWrapper.Object, _mobileStoreServiceLogger);
+        }
+
+        [Fact]
+        public void GetAllBrands_With_Success_Response()
+        {
+            //Arrange
+            ListResponse<Brand> expected = MobileStoreServiceTestData.GetAllBrandsSuccessResponse();
+
+            _mockIServiceWrapper.Setup(m => m.MobileStoreRepository.GetAllBrands())
+                               .Returns(MobileStoreServiceTestData.GetAllBrandDataAsync());
+
+            // Act
+            ListResponse<Brand> actual = _serviceWrapper.MobileStoreService.GetAllBrands().Result;
+
+            // Asert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.StatusCode, expected.StatusCode);
+            Assert.Equal(actual.Success, expected.Success);
+            Assert.Equal(JsonConvert.SerializeObject(actual.Response), JsonConvert.SerializeObject(expected.Response));
+        }
+
+        [Fact]
+        public void GetAllBrands_With_Null_Response()
+        {
+            // Arrange
+            ListResponse<Brand> expected = Utility.SendErrorResponseGetBrands(Constant.RECORD_NOT_FOUND);
+
+            _mockIServiceWrapper.Setup(m => m.MobileStoreRepository.GetAllBrands())
+                               .Returns(MobileStoreServiceTestData.HandleNullResponseForGetAllBrands());
+
+            // Act
+            ListResponse<Brand> actual = _serviceWrapper.MobileStoreService.GetAllBrands().Result;
+
+            // Asert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.StatusCode, expected.StatusCode);
+            Assert.Equal(actual.Success, expected.Success);
+            Assert.Equal(JsonConvert.SerializeObject(actual.Response), JsonConvert.SerializeObject(expected.Response));
+        }
+
+        [Fact]
+        public void GetAllBrands_Handle_Exception()
+        {
+            // Arrange
+            ListResponse<Brand> expected = Utility.SendErrorResponseGetBrands(Constant.ERROR_OCCURED);
+
+            _mockIServiceWrapper.Setup(m => m.MobileStoreRepository.GetAllBrands())
+                               .Returns(MobileStoreServiceTestData.HandleExceptionResponseForGetAllBrands());
+
+            // Act
+            ListResponse<Brand> actual = _serviceWrapper.MobileStoreService.GetAllBrands().Result;
+
+            // Asert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.StatusCode, expected.StatusCode);
+            Assert.Equal(actual.Success, expected.Success);
+            Assert.Equal(JsonConvert.SerializeObject(actual.Response), JsonConvert.SerializeObject(expected.Response));
+        }
+
+        /* Like above we can write the test cases for GetAllStocks, GetAllMobiles */
+
+        [Fact]
+        public void GetMobilesByModelName_With_Success_Response()
+        {
+            //Arrange
+            string modelName = "ABD";
+            ListResponse<Mobile> expected = MobileStoreServiceTestData.GetMobilesByModelNameWithSuccessResponse();
+
+            _mockIServiceWrapper.Setup(m => m.MobileStoreRepository.GetMobilesByModelName(It.IsAny<string>()))
+                               .Returns(MobileStoreServiceTestData.GetMobilesByModelNameAsync());
+
+            // Act
+            ListResponse<Mobile> actual = _serviceWrapper.MobileStoreService.GetMobilesByModelName(modelName).Result;
+
+            // Asert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.StatusCode, expected.StatusCode);
+            Assert.Equal(actual.Success, expected.Success);
+            Assert.Equal(JsonConvert.SerializeObject(actual.Response), JsonConvert.SerializeObject(expected.Response));
+        }
+
+        [Fact]
+        public void GetMobilesByModelName_With_Null_Response()
+        {
+            // Arrange
+            string modelName = "ABD";
+            ListResponse<Mobile> expected = Utility.SendErrorResponseGetMobiles(Constant.RECORD_NOT_FOUND);
+
+            _mockIServiceWrapper.Setup(m => m.MobileStoreRepository.GetMobilesByModelName(It.IsAny<string>()))
+                               .Returns(MobileStoreServiceTestData.HandleNullResponseForGetMobilesByModelName());
+
+            // Act
+            ListResponse<Mobile> actual = _serviceWrapper.MobileStoreService.GetMobilesByModelName(modelName).Result;
+
+            // Asert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.StatusCode, expected.StatusCode);
+            Assert.Equal(actual.Success, expected.Success);
+            Assert.Equal(JsonConvert.SerializeObject(actual.Response), JsonConvert.SerializeObject(expected.Response));
+        }
+
+        [Fact]
+        public void GetMobilesByModelName_Handle_Exception()
+        {
+            // Arrange
+            string modelName = "ABD";
+            ListResponse<Mobile> expected = Utility.SendErrorResponseGetMobiles(Constant.ERROR_OCCURED);
+
+            _mockIServiceWrapper.Setup(m => m.MobileStoreRepository.GetMobilesByModelName(It.IsAny<string>()))
+                               .Returns(MobileStoreServiceTestData.HandleExceptionResponseForGetMobilesByModelName());
+
+            // Act
+            ListResponse<Mobile> actual = _serviceWrapper.MobileStoreService.GetMobilesByModelName(modelName).Result;
+
+            // Asert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.StatusCode, expected.StatusCode);
+            Assert.Equal(actual.Success, expected.Success);
+            Assert.Equal(JsonConvert.SerializeObject(actual.Response), JsonConvert.SerializeObject(expected.Response));
+        }
     }
 }
--- FreedomMobileShop.Service.Test/TestCaseData/MobileStoreServiceTestData.cs
+++ FreedomMobileShop.Service.Test/TestCaseData/MobileStoreServiceTestData.cs
@@ -1,13 +1,159 @@
 ﻿/// <summary>
-/// 
+/// Test data for mobile store service test methods
 /// </summary>
 namespace FreedomMobileShop.Service.Test.TestCaseData
 {
-    using System;
+    using FreedomMobileShop.Common.Constants;
+    using FreedomMobileShop.Common.Helpers;
+    using FreedomMobileShop.Entity.Entities;
     using System.Collections.Generic;
-    using System.Text;
+    using System.Net;
+    using System.Threading.Tasks;
 
-    class MobileStoreServiceTestData
+    public class MobileStoreServiceTestData
     {
+        internal static ListResponse<Brand> GetAllBrandsSuccessResponse()
+        {
+            return new ListResponse<Brand>
+            {
+                Response = new List<Brand>
+                {
+                    new Brand
+                    {
+                       BrandID = 1,
+                       Name ="ABC",
+                       Company = "CDE",
+                       Type="XX",
+                       Description = "YYY"
+                    },
+                    new Brand
+                    {
+                       BrandID = 2,
+                       Name ="DEF",
+                       Company = "DSE",
+                       Type="HHH",
+                       Description = "jjj"
+                    }
+                },
+                Message = Constant.RECORD_FOUND,
+                StatusCode = HttpStatusCode.OK,
+                Success = true
+            };
+        }
+
+        internal static Task<IEnumerable<Brand>> GetAllBrandDataAsync()
+        {
+            return Task.Run(() => GetAllBrandData());
+        }
+
+        internal static IEnumerable<Brand> GetAllBrandData()
+        {
+            return new List<Brand>
+            {
+                new Brand
+                {
+                    BrandID = 1,
+                    Name ="ABC",
+                    Company = "CDE",
+                    Type="XX",
+                    Description = "YYY"
+                },
+                new Brand
+                {
+                    BrandID = 2,
+                    Name ="DEF",
+                    Company = "DSE",
+                    Type="HHH",
+                    Description = "jjj"
+                }
+            };
+        }
+
+        internal static Task<IEnumerable<Brand>> HandleNullResponseForGetAllBrands()
+        {
+            IEnumerable<Brand> data = new List<Brand>();
+            return Task.Run(() => data);
+        }
+
+        internal static Task<IEnumerable<Brand>> HandleExceptionResponseForGetAllBrands()
+        {
+            return null;
+        }
+
+        internal static ListResponse<Mobile> GetMobilesByModelNameWithSuccessResponse()
+        {
+            return new ListResponse<Mobile>
+            {
+                Response = new List<Mobile>
+                {
+                    new Mobile
+                    {
+                       MobileId = 1,
+                       MobileCompanyId = "AAA",
+                       IMEINumber ="121-1212-1212",
+                       Description ="Empty",
+                       Name = "ABD",
+                       Type = "TTT",
+                       MobileModelId = "NNN"
+                    },
+                    new Mobile
+                    {
+                        MobileId = 2,
+                        MobileCompanyId = "AAA",
+                        IMEINumber = "121-1212-1212",
+                        Description = "Empty",
+                        Name = "ABD",
+                        Type = "TTT",
+                        MobileModelId = "NNN"
+                    }
+                },
+                Message = Constant.RECORD_FOUND,
+                StatusCode = HttpStatusCode.OK,
+                Success = true
+            };
+        }
+
+        internal static Task<IEnumerable<Mobile>> GetMobilesByModelNameAsync()
+        {
+            return Task.Run(() => GetAllMobileWithSameModel());
+        }
+
+        internal static IEnumerable<Mobile> GetAllMobileWithSameModel()
+        {
+            return new List<Mobile>
+            {
+                new Mobile
+                {
+                    MobileId = 1,
+                    MobileCompanyId = "AAA",
+                    IMEINumber ="121-1212-1212",
+                    Description ="Empty",
+                    Name = "ABD",
+                    Type = "TTT",
+                    MobileModelId = "NNN"
+                },
+                new Mobile
+                {
+                    MobileId = 2,
+                    MobileCompanyId = "AAA",
+                    IMEINumber = "121-1212-1212",
+                    Description = "Empty",
+                    Name = "ABD",
+                    Type = "TTT",
+                    MobileModelId = "NNN"
+                }
+            };
+        }
+
+        internal static Task<IEnumerable<Mobile>> HandleNullResponseForGetMobilesByModelName()
+        {
+            IEnumerable<Mobile> data = new List<Mobile>();
+            return Task.Run(() => data);
+        }
+
+        internal static Task<IEnumerable<Mobile>> HandleExceptionResponseForGetMobilesByModelName()
+        {
+            return null;
+        }
     }
 }
